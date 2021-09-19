e36e49735fde097a97c2617b0ed9b80bb1b94f9b
shubham dixit <shubhamdixit863@gmail.com>
2021-09-19 20:05:47 +0530

    Export import

---
+++ exportsimports/first.js
@@ -0,0 +1,41 @@
+export function AddNumbers(num1 ,num2)
+{
+    // does some job  for adding the number
+    return num1+num2;
+}
+
+
+//  ---
+// Single export 
+export function SubtractNumber(num1,num2)
+{
+
+    return num1-num2;
+}
+
+
+
+export function MultiplyNumber(num1,num2)
+{
+
+    return num1*num2;
+}
+
+ function DivideNumber(num1,num2)
+{
+
+    return num1/num2;
+}
+
+export function ModulusNumber(num1,num2)
+{
+
+    return num1%num2;
+}
+
+// default export 
+export default DivideNumber;
+
+// we have to export it 
+
+//export default AddNumbers;
---
+++ exportsimports/second.js
@@ -0,0 +1,16 @@
+import { AddNumbers } from "./first.js";
+
+export const obj={
+    name:AddNumbers(2,9999),
+    url:""
+}
+
+
+const obj2={
+    value:"Something"
+}
+
+export const data="I love React";
+
+export default obj2;
+
