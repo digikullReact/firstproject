ae089b8ba421c0aa994756e62fb4c33fd5f02860
shubham dixit <shubhamdixit863@gmail.com>
2021-09-27 20:52:36 +0530

    Added a slider

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,51 +1,117 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 function Slider() {
+
+    const [state,setState]=useState({
+        img1:true,
+        img2:false,
+        img3:false
+
+    })
+
+
+    const [currentImage,setCurrentImage]=useState(1);
+
+
+    const previous=()=>{
+       
+
+        if(currentImage===3){
+
+            setState({img2:true,img1:false,img3:false})
+            setCurrentImage(currentImage-1)
+            
+        }
+        else if(currentImage===2){
+            setState({img2:false,img1:true,img3:false})
+            setCurrentImage(currentImage-1)
+
+
+        }
+    
+
+
+ 
+
+
+
+    }
+
+    const next=()=>{
+
+     console.log(currentImage);
+        debugger;
+
+        if(currentImage===1){
+
+            setState({img2:true,img1:false,img3:false})
+            setCurrentImage(currentImage+1)
+            
+        }
+        else if(currentImage===2){
+            setState({img2:false,img1:false,img3:true})
+            setCurrentImage(currentImage+1)
+
+
+        }
+    
+      
+
+    }
+
+
+
+
+  
+
     return (
         <>
        
 <div id="myCarousel" className="carousel slide">
  
-  <ol className="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol>
+ 
   <div className="carousel-inner">
     <div className="item active">
-      <img src="/assets/example/bg_suburb.jpg" className="img-responsive"/>
-      <div className="container">
-        <div className="carousel-caption">
-          <h1>Bootstrap 3 Carousel Layout</h1>
-        
-      </div>
+     
+
+     {
+         state.img1? <div className="item">
+         <img  src="https://source.unsplash.com//1500x600/?dogs" className="img-responsive"/>
+         <div className="container">
+           <div className="carousel-caption">
+            
+           </div>
+         </div>
+       </div>:""
+
+     }
+
+     {
+         state.img2?    <div className="item">
+         <img src="https://source.unsplash.com//1500x600/?nature"className="img-responsive"/>
+         <div className="container">
+         
+         </div>
+       </div>:""
+     }
+   
+
+{
+    state.img3?<div className="item">
+    <img src="https://source.unsplash.com//1500x600/?cat"className="img-responsive"/>
+    <div className="container">
+    
     </div>
-    <div className="item">
-      <img src="http://lorempixel.com/1500/600/abstract/1" className="img-responsive"/>
-      <div className="container">
-        <div className="carousel-caption">
-          <h1>Changes to the Grid</h1>
-          <p>Bootstrap 3 still features a 12-column grid, but many of the CSS className names have completely changed.</p>
-          <p><a className="btn btn-large btn-primary" href="#">Learn more</a></p>
-        </div>
-      </div>
-    </div>
-    <div className="item">
-      <img src="http://placehold.it/1500X500" className="img-responsive"/>
-      <div className="container">
-        <div className="carousel-caption">
-          <h1>Percentage-based sizing</h1>
-          <p>With "mobile-first" there is now only one percentage-based grid.</p>
-          <p><a className="btn btn-large btn-primary" href="#">Browse gallery</a></p>
-        </div>
-      </div>
-    </div>
+  </div>:""
+}
+
+    
   </div>
 
-  <a className="left carousel-control" href="#myCarousel" data-slide="prev">
+  <a className="left carousel-control" >
     <span className="icon-prev"></span>
   </a>
-  <a className="right carousel-control" href="#myCarousel" data-slide="next">
+  <a className="right carousel-control" >
     <span className="icon-next"></span>
   </a>  
 </div>
@@ -53,6 +119,16 @@ function Slider() {
 
 
 </div>
+<button className="btn btn-success"  onClick={previous}>
+
+Previous
+</button>
+
+
+<button className="btn btn-success" onClick={next}>
+
+    Next
+</button>
 
 </>
     )
